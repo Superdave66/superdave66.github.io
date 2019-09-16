3b449ef29b06fb6f72c72a6599819194b3f78162
SPC David Wiliams II <davidwilliams1102@yahoo.com>
2019-09-16 15:46:37 -0500

    intial commit working on generating name code

---
+++ dice game/dice game.js
@@ -0,0 +1,12 @@
+function rungame(){   //runs the game. note succesfull winning of game is almost impossible 
+prompt("please choose your gender")  // determine if male or female
+let M =1 
+let f=2
+	function namegenarator(x){
+
+	}	function rolldice(x){
+											//x= number of sides of dice being used
+		math.floor(math.random()* x)
+		console.log (rolldice(4))
+	}
+}
