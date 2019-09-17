7eff423f76ae6fb24148951e0ee721c7cc7cf4b7
SPC David Wiliams II <davidwilliams1102@yahoo.com>
2019-09-17 11:31:00 -0500

    name generates working on acceptance of deal

--- dice game/dice game.js
+++ dice game/dice game.js
@@ -1,12 +1,22 @@
-function rungame(){   //runs the game. note succesfull winning of game is almost impossible 
-prompt("please choose your gender")  // determine if male or female
-let M =1 
-let f=2
-	function namegenarator(x){
+function rungame(){ 
+namegenerator();  //runs the game. note succescfull completion  of game is almost impossible 
 
-	}	function rolldice(x){
-											//x= number of sides of dice being used
-		math.floor(math.random()* x)
-		console.log (rolldice(4))
-	}
 }
+function namegenerator(name){
+	let array_character = [["Ted", "Jeffery", "Ed", "Jack"],[ "Amy", "faye", "Suzan", "Tillie"]];
+	let gender  = prompt("type 0 for male type 1 for female ");
+
+	let genderArray = array_character[gender];
+	let i= rolldice(4) - 1;
+    let namegenerated = genderArray[i]; 
+	console.log(namegenerated);
+}
+function rolldice(x){										//x= number of sides of dice being used with 0 and 1 of the sides 
+	return Math.floor(Math.random() * x) + 1;
+}	
+ // function acceptanceofdeal(answer)
+	// let acceptance = prompt("will you accept the offer");
+ // 	console.log(acceptanceofdeal(acceptance);
+
+						// prompt to ask if you want to accept the offer or not. if not go to game over screen if accepted go to first encounter 
+rungame()
